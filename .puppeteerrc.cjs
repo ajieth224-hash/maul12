dd02ab629011df41fe42421034cda4098f2383aa
ajieth224-hash <ajie.th224@gmail.com>
2026-07-30 18:36:55 +0700

    Remove comment from Puppeteer configuration file
    Removed comment about Puppeteer installation directory.

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,7 +4,5 @@ const {join} = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Mengarahkan Puppeteer untuk menginstal Chrome di dalam folder proyek ini
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
- g
